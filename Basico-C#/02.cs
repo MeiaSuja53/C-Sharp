85ba06248103c46857dba8ac3cd4944da5953abd
Felipe Alves <141966328+MeiaSuja53@users.noreply.github.com>
2025-07-20 23:49:02 -0300

    Add files via upload

---
+++ Basico-C#/02.cs
@@ -0,0 +1,12 @@
+﻿namespace A01;
+
+public class Program
+{
+    public static void Main()
+    {
+        int quantidade = 20;
+        string gaveta = "Meia";
+
+        Console.WriteLine(quantidade + gaveta);
+    }
+}
